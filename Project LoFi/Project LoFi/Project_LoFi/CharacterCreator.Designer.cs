94bdd235ba6e946bea2f95b985cc55de6cec841c
Phillip Fowler <paf9237@rit.edu>
2014-10-29 17:58:36 -0400

    Created 'CharacterCreator' Form class Added design to form

---
+++ Project LoFi/Project LoFi/Project_LoFi/CharacterCreator.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Project_LoFi
+{
+    partial class CharacterCreator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PreviousPortrait = new System.Windows.Forms.Button();
+            this.NextPortrait = new System.Windows.Forms.Button();
+            this.Portrait = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.NextMember = new System.Windows.Forms.Button();
+            this.PreviousMember = new System.Windows.Forms.Button();
+            this.PointsLeft = new System.Windows.Forms.ProgressBar();
+            this.PointLabel = new System.Windows.Forms.Label();
+            this.StrengthLabel = new System.Windows.Forms.Label();
+            this.Stat1 = new System.Windows.Forms.NumericUpDown();
+            this.Stat2 = new System.Windows.Forms.NumericUpDown();
+            this.Dexterity = new System.Windows.Forms.Label();
+            this.Stat3 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Stat4 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HealthLabel = new System.Windows.Forms.Label();
+            this.Health = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 126);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PreviousPortrait
+            // 
+            this.PreviousPortrait.Location = new System.Drawing.Point(13, 146);
+            this.PreviousPortrait.Name = "PreviousPortrait";
+            this.PreviousPortrait.Size = new System.Drawing.Size(59, 23);
+            this.PreviousPortrait.TabIndex = 1;
+            this.PreviousPortrait.Text = "<--";
+            this.PreviousPortrait.UseVisualStyleBackColor = true;
+            // 
+            // NextPortrait
+            // 
+            this.NextPortrait.Location = new System.Drawing.Point(79, 146);
+            this.NextPortrait.Name = "NextPortrait";
+            this.NextPortrait.Size = new System.Drawing.Size(59, 23);
+            this.NextPortrait.TabIndex = 2;
+            this.NextPortrait.Text = "-->";
+            this.NextPortrait.UseVisualStyleBackColor = true;
+            // 
+            // Portrait
+            // 
+            this.Portrait.AutoSize = true;
+            this.Portrait.Location = new System.Drawing.Point(55, 172);
+            this.Portrait.Name = "Portrait";
+            this.Portrait.Size = new System.Drawing.Size(40, 13);
+            this.Portrait.TabIndex = 3;
+            this.Portrait.Text = "Portrait";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(188, 13);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 4;
+            this.NameLabel.Text = "Name";
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(162, 30);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(225, 20);
+            this.Name.TabIndex = 5;
+            // 
+            // NextMember
+            // 
+            this.NextMember.Location = new System.Drawing.Point(288, 432);
+            this.NextMember.Name = "NextMember";
+            this.NextMember.Size = new System.Drawing.Size(110, 40);
+            this.NextMember.TabIndex = 6;
+            this.NextMember.Text = "Next Character";
+            this.NextMember.UseVisualStyleBackColor = true;
+            // 
+            // PreviousMember
+            // 
+            this.PreviousMember.Location = new System.Drawing.Point(12, 432);
+            this.PreviousMember.Name = "PreviousMember";
+            this.PreviousMember.Size = new System.Drawing.Size(110, 40);
+            this.PreviousMember.TabIndex = 7;
+            this.PreviousMember.Text = "Previous Character";
+            this.PreviousMember.UseVisualStyleBackColor = true;
+            // 
+            // PointsLeft
+            // 
+            this.PointsLeft.Location = new System.Drawing.Point(12, 349);
+            this.PointsLeft.Name = "PointsLeft";
+            this.PointsLeft.Size = new System.Drawing.Size(385, 23);
+            this.PointsLeft.TabIndex = 8;
+            // 
+            // PointLabel
+            // 
+            this.PointLabel.AutoSize = true;
+            this.PointLabel.Location = new System.Drawing.Point(32, 333);
+            this.PointLabel.Name = "PointLabel";
+            this.PointLabel.Size = new System.Drawing.Size(63, 13);
+            this.PointLabel.TabIndex = 9;
+            this.PointLabel.Text = "Points Left: ";
+            // 
+            // StrengthLabel
+            // 
+            this.StrengthLabel.AutoSize = true;
+            this.StrengthLabel.Location = new System.Drawing.Point(165, 125);
+            this.StrengthLabel.Name = "StrengthLabel";
+            this.StrengthLabel.Size = new System.Drawing.Size(47, 13);
+            this.StrengthLabel.TabIndex = 10;
+            this.StrengthLabel.Text = "Strength";
+            // 
+            // Stat1
+            // 
+            this.Stat1.Location = new System.Drawing.Point(266, 125);
+            this.Stat1.Name = "Stat1";
+            this.Stat1.Size = new System.Drawing.Size(120, 20);
+            this.Stat1.TabIndex = 11;
+            // 
+            // Stat2
+            // 
+            this.Stat2.Location = new System.Drawing.Point(266, 172);
+            this.Stat2.Name = "Stat2";
+            this.Stat2.Size = new System.Drawing.Size(120, 20);
+            this.Stat2.TabIndex = 13;
+            // 
+            // Dexterity
+            // 
+            this.Dexterity.AutoSize = true;
+            this.Dexterity.Location = new System.Drawing.Point(165, 172);
+            this.Dexterity.Name = "Dexterity";
+            this.Dexterity.Size = new System.Drawing.Size(48, 13);
+            this.Dexterity.TabIndex = 12;
+            this.Dexterity.Text = "Dexterity";
+            // 
+            // Stat3
+            // 
+            this.Stat3.Location = new System.Drawing.Point(266, 224);
+            this.Stat3.Name = "Stat3";
+            this.Stat3.Size = new System.Drawing.Size(120, 20);
+            this.Stat3.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Magic";
+            // 
+            // Stat4
+            // 
+            this.Stat4.Location = new System.Drawing.Point(266, 275);
+            this.Stat4.Name = "Stat4";
+            this.Stat4.Size = new System.Drawing.Size(120, 20);
+            this.Stat4.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Endurance";
+            // 
+            // HealthLabel
+            // 
+            this.HealthLabel.AutoSize = true;
+            this.HealthLabel.Location = new System.Drawing.Point(12, 250);
+            this.HealthLabel.Name = "HealthLabel";
+            this.HealthLabel.Size = new System.Drawing.Size(44, 13);
+            this.HealthLabel.TabIndex = 18;
+            this.HealthLabel.Text = "Health: ";
+            // 
+            // Health
+            // 
+            this.Health.Location = new System.Drawing.Point(63, 250);
+            this.Health.Name = "Health";
+            this.Health.Size = new System.Drawing.Size(59, 20);
+            this.Health.TabIndex = 19;
+            // 
+            // CharacterCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 484);
+            this.Controls.Add(this.Health);
+            this.Controls.Add(this.HealthLabel);
+            this.Controls.Add(this.Stat4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Stat3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Stat2);
+            this.Controls.Add(this.Dexterity);
+            this.Controls.Add(this.Stat1);
+            this.Controls.Add(this.StrengthLabel);
+            this.Controls.Add(this.PointLabel);
+            this.Controls.Add(this.PointsLeft);
+            this.Controls.Add(this.PreviousMember);
+            this.Controls.Add(this.NextMember);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.Portrait);
+            this.Controls.Add(this.NextPortrait);
+            this.Controls.Add(this.PreviousPortrait);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "CharacterCreator";
+            this.Text = "CharacterCreator";
+            this.Load += new System.EventHandler(this.CharacterCreator_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Stat4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button PreviousPortrait;
+        private System.Windows.Forms.Button NextPortrait;
+        private System.Windows.Forms.Label Portrait;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.Button NextMember;
+        private System.Windows.Forms.Button PreviousMember;
+        private System.Windows.Forms.ProgressBar PointsLeft;
+        private System.Windows.Forms.Label PointLabel;
+        private System.Windows.Forms.Label StrengthLabel;
+        private System.Windows.Forms.NumericUpDown Stat1;
+        private System.Windows.Forms.NumericUpDown Stat2;
+        private System.Windows.Forms.Label Dexterity;
+        private System.Windows.Forms.NumericUpDown Stat3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown Stat4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label HealthLabel;
+        private System.Windows.Forms.TextBox Health;
+    }
+}
---
+++ Project LoFi/Project LoFi/Project_LoFi/CharacterCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project_LoFi
+{
+    public partial class CharacterCreator : Form
+    {
+        public CharacterCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void CharacterCreator_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
