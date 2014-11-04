0f41d982b58e208e355b2acfbee1abefb279d0c8
Darren <dmf6737@rit.edu>
2014-11-04 07:33:08 -0500

    Finished coding the current functions in EnemyAI.

--- Project LoFi/Project LoFi/Project_LoFi/EnemyAI.cs
+++ Project LoFi/Project LoFi/Project_LoFi/EnemyAI.cs
@@ -17,20 +17,36 @@ namespace Project_LoFi
         }
 
         //determine if the computer should attack a player
-        public bool AttackPlayer()
+        public bool AttackPlayer(EnemyUnit enemy, PlayerUnit player)
         {
+            //if the player is right next to the enemy unit return true
+            if( ((player.X - 1) == enemy.X || player.X + 1 == enemy.X || player.X == enemy.X) &&
+                ((player.Y - 1) == enemy.Y || player.Y + 1 == enemy.Y || player.Y == enemy.Y) )
+            {
+                return true;
+            }
             return false;
         }
 
         //determine if the computer should move towards a player
-        public bool MoveTowardsPlayer()
+        public bool MoveTowardsPlayer(EnemyUnit enemy, PlayerUnit player)
         {
+            //check to see if the enemy can see the player and if are they in a straight line path
+            if(IsPlayerVisible(enemy, player) && (player.X == enemy.X || player.Y == enemy.Y))
+            {
+                return true;
+            }
             return false;
         }
 
         //determine if the computer should chase after a retreating player
-        public bool ChasePlayer()
+        public bool ChasePlayer(EnemyUnit enemy, PlayerUnit player)
         {
+            //if players life is less then half of enemies they will pursue the player
+            if(player.Health < (enemy.Health / 2))
+            {
+                return true;
+            }
             return false;
         }
 
@@ -40,10 +56,11 @@ namespace Project_LoFi
             return false;
         }
 
-        //can the enemy see a player
-        public bool IsPlayerVisible(int enemyNum, PlayerUnit player)
+        //can the enemy see or hear a player nearby
+        public bool IsPlayerVisible(EnemyUnit enemy, PlayerUnit player)
         {
-            if ( (Math.Abs((player.X - enemyList[enemyNum].X))) < 5 && (Math.Abs((player.Y - enemyList[enemyNum].Y))) < 5 )
+            //if player is within 4 spaces of the enemy it is considered seen or heard
+            if ((Math.Abs((player.X - enemy.X))) < 5 && (Math.Abs((player.Y - enemy.Y))) < 5)
             {
                 return true;
             }
@@ -51,31 +68,34 @@ namespace Project_LoFi
         }
 
         //find closest player
-        public PlayerUnit ClosestPlayer(int enemyNum)
+        public PlayerUnit ClosestPlayer(EnemyUnit enemy)
         {
-            int x = 100;
-            int y;
+            //set x, y coordinates to some number that could never be on the map
+            int x = 1000;
+            int y = 1000;
             int pNum = 0;
 
-            if( (Math.Abs((characterList[0].X - enemyList[enemyNum].X))) < x && (Math.Abs((characterList[0].Y - enemyList[enemyNum].Y))) < y)
+            //check how close the player is to the enemy and store new x,y coords
+            if( (Math.Abs((characterList[0].X - enemy.X))) < x && (Math.Abs((characterList[0].Y - enemy.Y))) < y)
             {
-                x = Math.Abs((characterList[0].X - enemyList[enemyNum].X));
-                y = Math.Abs((characterList[0].Y - enemyList[enemyNum].Y));
+                x = Math.Abs((characterList[0].X - enemy.X));
+                y = Math.Abs((characterList[0].Y - enemy.Y));
                 pNum = 0;
             }
-            if ((Math.Abs((characterList[1].X - enemyList[enemyNum].X))) < x && (Math.Abs((characterList[1].Y - enemyList[enemyNum].Y))) < y)
+            if ((Math.Abs((characterList[1].X - enemy.X))) < x && (Math.Abs((characterList[1].Y - enemy.Y))) < y)
             {
-                x = Math.Abs((characterList[1].X - enemyList[enemyNum].X));
-                y = Math.Abs((characterList[1].Y - enemyList[enemyNum].Y));
+                x = Math.Abs((characterList[1].X - enemy.X));
+                y = Math.Abs((characterList[1].Y - enemy.Y));
                 pNum = 1;
             }
-            if ((Math.Abs((characterList[2].X - enemyList[enemyNum].X))) < x && (Math.Abs((characterList[2].Y - enemyList[enemyNum].Y))) < y)
+            if ((Math.Abs((characterList[2].X - enemy.X))) < x && (Math.Abs((characterList[2].Y - enemy.Y))) < y)
             {
-                x = Math.Abs((characterList[2].X - enemyList[enemyNum].X));
-                y = Math.Abs((characterList[2].Y - enemyList[enemyNum].Y));
+                x = Math.Abs((characterList[2].X - enemy.X));
+                y = Math.Abs((characterList[2].Y - enemy.Y));
                 pNum = 2;
             }
 
+            //return character closest to the enemy
             return characterList[pNum];
         }//end ClosestPlayer
 
